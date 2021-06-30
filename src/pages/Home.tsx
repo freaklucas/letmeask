d750fe2d754e8baaeab947340529cb8a90c87f3c
freaklucas <lucas.oliveira.freak@gmail.com>
2021-06-30 01:25:16 -0300

    criando hook +  incrementando tela de admin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,6 +38,13 @@ export function Home() {
             
             return;
         }
+
+        if (roomRef.val().endedAt) {
+            alert('Room Already closed.');
+
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`);
     }   
 
