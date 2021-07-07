eb34ad307a171f53f70c8c6daa5d989df30ae7b5
freaklucas <lucas.oliveira.freak@gmail.com>
2021-07-07 00:29:14 -0300

    adicionando dark mode unitario

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,35 @@
+import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIcon from '../assets/images/google-icon.svg';
 
-
-import{ Button } from '../components/Button';
+import { Button } from '../components/Button';
 import { useAuth } from '../hooks/UseAuth';
 
 import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
-import { FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
+import { Toggle } from "../components/Toggle";
+import { useDarkMode } from "../styles/darkMode/useDarkMode";
+import { GlobalStyles, lightTheme, darkTheme } from "../styles/darkMode/theme";
+import styled, { ThemeProvider } from 'styled-components';
 
 export function Home() {
     const history = useHistory(); // é um hook e deve ficar sempre dentro do contexto de execução do componente em questão
     const { signInWithGoogle, user } = useAuth();
     const [roomCode, setRoomCode] = useState('');
+
+
+    const Container = styled.div`
+        max-width: 50%;
+        margin: 8rem auto 0;
+    `;
+
+    const [theme, toggleTheme] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
     async function handleCreateRoom() {
         if (!user) {
@@ -35,7 +48,7 @@ export function Home() {
 
         if (!roomRef.exists()) {
             alert('Room does not exists.');
-            
+
             return;
         }
 
@@ -46,21 +59,29 @@ export function Home() {
         }
 
         history.push(`/rooms/${roomCode}`);
-    }   
+    }
 
     return (
         <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Imagem simbolizando perguntas e respostas" />
-                    <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                    <p>Tire as dúvidas da galera em tempo-real</p>
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da galera em tempo-real</p>
             </aside>
             <main>
                 <div className="main-content">
-                    <img src={ logoImg } alt="Imagem simbolizando uma logo da aplicação Letmeask" />
-                    <button onClick={handleCreateRoom}
+                    <img src={logoImg} alt="Imagem simbolizando uma logo da aplicação Letmeask" />
+                    <div>
+                        <ThemeProvider theme={themeMode}>
+                            <Container>
+                                <GlobalStyles />
+                                <Toggle theme={theme} toggleTheme={toggleTheme} />
+                            </Container>
+                        </ThemeProvider>
+                    </div>
+                    <button onClick={handleCreateRoom}  
                         className="create-room">
-                        <img src={ googleIcon } alt="Logo do google" />
+                        <img src={googleIcon} alt="Logo do google" />
                         Crie sua sala com Google
                     </button>
                     <div className="separator">
